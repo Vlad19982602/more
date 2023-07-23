f0c80179a6cf6829a1caedb94446e54ae8be5a0b
Vladislav Fedorov <ma3za@list.ru>
2023-07-23 13:12:09 +0500

    mask form active

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 import scrolling from './modules/pageup.js';
+import mask from './modules/mask.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 
 	scrolling('.pageup');
+	mask('[name="form_text_2"]');
 });
 
